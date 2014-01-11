5bf379863615b768b192d601fc71645acea2c24d
Alexander Chernyakhovsky <achernya@mit.edu>
2014-01-11 18:25:12 -0500

    Ship a setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name='get_message',
+      version='10.1',
+      description='Athena utility for getting the global message of the day',
+      author='Alexander Chernyakhovsky',
+      scripts=['get_message']
+      )
