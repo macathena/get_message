e4a6814cdea59dea82f2eee5f2d39da6e83b1e93
Jonathan Reed <jdreed@mit.edu>
2014-04-10 11:24:27 -0400

    Support ancient long arguments; bump to 10.1.1
    Because it was a good idea in 1980-something, get_message
    is in lots of places with non-GNU-style long arguments.  Include
    a dumb hack to support these.
    
    Also, support the -z argument, while noting that it's deprecated.
    
    Release this as 10.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='get_message',
-      version='10.1',
+      version='10.1.1',
       description='Athena utility for getting the global message of the day',
       author='Alexander Chernyakhovsky',
       scripts=['get_message']
